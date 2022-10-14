c75ef126dda4d7c70c030ee72575b35e9189fd57
lushuai <87168182+biolushuai@users.noreply.github.com>
2022-10-14 09:20:55 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,32 +1,31 @@
 class DefaultConfig(object):
-    acid_one_hot = [0 for i in range(20)]
-    acid_idx = {j: i for i, j in enumerate("ACDEFGHIKLMNPQRSTVWY")}
-    acid_idx2 = {i: j for i, j in enumerate("ACDEFGHIKLMNPQRSTVWY")}
+    train_dataset_path = r'./data/deepppisp_test.pkl'
+    test_dataset_path = r'./data/pipenn_zk448_a_52.pkl'
+    # test_dataset_path = r'./data/deepppisp_test_52.pkl'
+    save_path = r'./models_saved/pipenn_a_feat52_att32_cnn64_k357_w5_e100_c10_b128_lr0.001_dp0.2_ng0.1_sp0.8'
 
-    BASE_PATH = "../../"
-    sequence_path = "{0}/data_cache/sequence_data".format(BASE_PATH)
-    pssm_path = "{0}/data_cache/pssm_data".format(BASE_PATH)
-    dssp_path = "{0}/data_cache/dssp_data".format(BASE_PATH)
-    dssp2_path = "{0}/data_cache/dssp2_data".format(BASE_PATH)
-    rasa_path = "{0}/data_cache/rasa_data".format(BASE_PATH)
-    angle_path = "{0}/data_cache/angle_data".format(BASE_PATH)
-
-    save_path = r'D:\PythonProjects\InteractionSitePrediction\saved_models\ppi_feat52_local_cnnattention_t1_v5_e250_w5_b32_dp0.2_sp0.9'
-    data_sets = ["dset186", "dset164", "dset72"]
-
-    epochs = 250
+    epochs = 100
     batch_size = 32
-    learning_rate = 0.001
-    feature_dim = 52
-    dropout = 0.2
-    split_rate = 0.9
 
-    attention_dim = 56
-    local_kernels = [3, 5, 7]
-    window_size = 5
+    learning_rate = 0.001
+    weight_decay = 5e-4
+    dropout_rate = 0.2
+    # neg_wt = 0.06
+    split_rate = 0.8
+
+    #  input layer
+    feature_dim = 52
+    window_padding_size = 2
+
+    # attention layer
+    attention_hidden_dim = 32
+
+    # cnn layer
+    kernels = [3, 5, 7]
     out_channel = 64
 
-    max_sequence_length = 500
+    # mlp
+    mlp_dim = 512
 
 
 
